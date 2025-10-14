d3cab3ee6022c80c84cb46298ec513d00c720a65
Bright Chen <1024347104@qq.com>
2025-10-14 15:02:04 +0800

    配置优化

--- cms-backend/main.py
+++ cms-backend/main.py
@@ -25,7 +25,7 @@ async def custom_swagger_ui_html():
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
